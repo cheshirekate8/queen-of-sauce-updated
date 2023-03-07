64f1ea3de7f754d9a630e32bb039298e337d6ca1
Katie Young <katie@sandstone.la>
2023-03-06 17:47:22 -0800

    simple css and readme cleanup

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import Header from "./Header";
+import Head from "next/head";
 
 type Props = {
   children: ReactNode;
@@ -7,6 +8,9 @@ type Props = {
 
 const Layout: React.FC<Props> = (props) => (
   <div>
+    <Head>
+      <title>My page title</title>
+    </Head>
     <Header />
     <div className="layout">{props.children}</div>
   </div>
